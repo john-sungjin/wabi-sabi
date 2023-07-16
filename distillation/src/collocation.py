e1f4b4f29954080e42a5c41f7ca7cadf78740694
John Sungjin Kim <jsjkim00@gmail.com>
2023-07-16 18:00:23 +0000

    collocation experiments

--- distillation/src/collocation.py
+++ distillation/src/collocation.py
@@ -20,11 +20,12 @@ root_dir = "/datadrive/wabi-sabi/distillation/src/"
 student_config = os.path.join(root_dir, "student.yaml")
 seed = 42
 
-run_name = "test1"
+run_name = "test7_no_teacher"
 run_dir = os.path.join(root_dir, "runs", run_name)
 save_folder = os.path.join(run_dir, "checkpoints")
-max_duration = "1000ba"
-save_interval = "200ba"
+# max_duration = "1000ba"
+max_duration = "36000ba"
+save_interval = "1000ba"
 
 torch.manual_seed(seed)
 random.seed(seed)
@@ -118,23 +119,67 @@ class StudentModel(composer.models.HuggingFaceModel):
         )
 
         self.n_active_params = sum(p.numel() for p in self.parameters())
-        self.loss_fn = torch.nn.MSELoss()
+        # self.loss_fn = torch.nn.KLDivLoss()
 
     def forward(self, batch):
+        # print(batch["logits"])
+        # print logits stats
+        # print(
+        #     "Input IDs: min {}, max {}, mean {}, std {}".format(
+        #         torch.min(batch["logits"]),
+        #         torch.max(batch["logits"]),
+        #         torch.mean(batch["logits"]),
+        #         torch.std(batch["logits"]),
+        #     )
+        # )
+
         return self.model(
             input_ids=torch.squeeze(batch["input_ids"]),  # squeeze because batch size 1
             attention_mask=torch.squeeze(batch["attention_mask"]),
         )
 
     def loss(self, outputs, batch):
-        mask = torch.permute(batch["attention_mask"], (1, 2, 0)) == 1
-        logits = outputs.logits.masked_select(mask)
-        targets = torch.squeeze(batch["logits"]).masked_select(mask)
+        # this permutation is a bit odd, but it's to make it broadcastable over the logits
+        # we get batch x seq_len x 1, but we want batch x seq_len x vocab_size
+        # batch["attention_mask"] is 1 where there isn't padding
+        # so we flip for the mask
+        mask = torch.permute(batch["attention_mask"], (1, 2, 0)) == 0
 
-        return self.loss_fn(targets, logits)
+        # 1. mask out padding tokens
+        logits = outputs.logits.masked_fill(mask, float("-inf"))
+        targets = torch.squeeze(batch["logits"]).masked_fill(mask, float("-inf"))
+
+        # 2. softmax to make a probability distribution
+        # needs to be log because kl_div expects it
+        # targets are not expected to be log but we'll do it anyway
+        logits = torch.nn.functional.log_softmax(logits, dim=-1)
+        targets = torch.nn.functional.log_softmax(targets, dim=-1)
+
+        # 3. compute KL-Divergence along last dimension
+        # we want to average over the batch and sequence length
+        # we want to do reverse KL-Divergence
+        # https://dibyaghosh.com/blog/probability/kldivergence.html
+        # https://arxiv.org/abs/2306.08543
+        # kl_div = torch.nn.functional.kl_div(
+        #     targets, logits, reduction="none", log_target=True
+        # )
+        kl_div = torch.nn.functional.kl_div(
+            logits, targets, reduction="none", log_target=True
+        )
+        # ensuring 0
+        kl_div = kl_div.masked_fill(mask, 0.0)
+        # sum along last dimension
+        kl_div = kl_div.sum(dim=-1)
+        # need to average with number of non-padding tokens in the seq_len dimension
+        kl_div = kl_div.sum(dim=-1) / (~mask).sum()
+        # finally, average over the batch
+        loss = kl_div.mean()
+
+        return loss
 
 
-student_model = StudentModel(om_model_config=yaml_cfg.model, tokenizer=tokenizer)
+# student_model = StudentModel(om_model_config=yaml_cfg.model, tokenizer=tokenizer)
+student_model = llmfoundry.ComposerMPTCausalLM(yaml_cfg, tokenizer)
 
 # In[]
 tinystories_data = datasets.load_from_disk(data_dir).with_format("torch")
@@ -158,7 +203,7 @@ def tokenize_function(examples: dict[str, Any]):
         examples[text_column_name],
         return_special_tokens_mask=True,
         padding=False,
-        max_length=teacher_config.max_seq_len,
+        max_length=max_seq_len,
         truncation=True,
     )
 
@@ -257,7 +302,8 @@ lr_scheduler = llmfoundry.utils.builders.build_scheduler(yaml_cfg.scheduler)
 wandb_logger = composer.loggers.WandBLogger(project="distillation")
 trainer = composer.Trainer(
     model=student_model,  # This is the model from the HuggingFaceModel wrapper class.
-    train_dataloader=student_loader,
+    # train_dataloader=student_loader,
+    train_dataloader=teacher_dataloader,
     max_duration=max_duration,  # train for more epochs to get better performance
     optimizers=optimizer,
     schedulers=[lr_scheduler],
--- distillation/src/test_model.py
+++ distillation/src/test_model.py
@@ -1,7 +1,7 @@
 # %%
 import transformers
 
-model_path = "/datadrive/wabi-sabi/distillation/src/runs/test1/hf_model"
+model_path = "/datadrive/wabi-sabi/distillation/src/runs/test7_no_teacher/hf_model"
 
 model = transformers.AutoModelForCausalLM.from_pretrained(
     model_path, trust_remote_code=True
@@ -9,13 +9,16 @@ model = transformers.AutoModelForCausalLM.from_pretrained(
 tokenizer = transformers.AutoTokenizer.from_pretrained(model_path)
 tokenizer.pad_token = tokenizer.eos_token  # doesn't have a pad token
 
-prompt = "There was a girl named"
+# %%
+prompt = "There was a boy named Jack. He was a good boy, and he was very good at sports. One day,"
 prompt_ids = tokenizer.encode(prompt, return_tensors="pt")
 
 print("Prompt IDs:", prompt_ids)
 
-output_ids = model.generate(prompt_ids, max_new_tokens=50)
+output_ids = model.generate(prompt_ids, max_new_tokens=200)
 print("Output IDs:", output_ids)
 
 output_text = tokenizer.decode(output_ids[0])
 print("Output Text:", output_text)
+
+# %%
